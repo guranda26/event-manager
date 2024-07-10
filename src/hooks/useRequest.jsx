45d2df945a3106d8131195db0bdb3d2bd4bc138a
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-07-10 20:39:23 +0400

    update useRequest

--- src/hooks/useRequest.jsx
+++ src/hooks/useRequest.jsx
@@ -1,29 +1,44 @@
 import { useState } from "react";
+
 const useRequest = ({ url, method }) => {
   const [loading, setLoading] = useState(false);
-  // const REACT_APP_API_KEY =
-  //   "zemvDlvtavG4trFqzcyYlOX0SHH_-hx8_z7YG56INKw9OFLh6A";
-
   const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
-  console.log(process.env.REACT_APP_API_KEY);
 
   const sendRequest = async (body, del) => {
     setLoading(true);
-    const res = await fetch(url || del, {
-      method,
-      headers: {
-        "Content-type": "/application/json",
-        Authorization: `Bearer ${REACT_APP_API_KEY}`,
-      },
-      body: !!body && body !== "GET" ? JSON.stringify(body) : undefined,
-    });
+    try {
+      const res = await fetch(url || del, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${REACT_APP_API_KEY}`,
+        },
+        body: !!body && body !== "GET" ? JSON.stringify(body) : undefined,
+      });
 
-    const data = await res.json();
-    setLoading(false);
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
 
-    return data;
+      const contentType = res.headers.get("content-type");
+      if (contentType && contentType.includes("application/json")) {
+        const data = await res.json();
+        setLoading(false);
+        return data;
+      } else {
+        const text = await res.text();
+        console.warn(`Received non-JSON response: ${text}`);
+        setLoading(false);
+        return null;
+      }
+    } catch (error) {
+      console.error("Error sending request:", error);
+      setLoading(false);
+      throw error;
+    }
   };
 
   return { loading, sendRequest };
 };
+
 export default useRequest;
