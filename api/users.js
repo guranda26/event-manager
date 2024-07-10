4a84b63252033ef4393edb3da75b065f2e3780b4
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-07-10 21:06:33 +0400

    create api

---
+++ api/users.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://event-manager-f4x7p1zv2-guranda26s-projects.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST", "DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "GET" || req.method === "GET") {
+    res.status(200).json({ users: [] });
+  }
+}
