ef8c12137039855b4f627546dbc0a15b66e9ee19
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-07-10 22:12:23 +0400

    add delete method

--- api/users.js
+++ api/users.js
@@ -6,7 +6,11 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST", "DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  if (req.method === "GET" || req.method === "POST") {
+  if (
+    req.method === "GET" ||
+    req.method === "POST" ||
+    req.method === "DELETE"
+  ) {
     res.status(200).json({ users: [] });
   }
 }
